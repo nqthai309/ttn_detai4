a4695c5151dfd5d2b4bb21ed6c7a843dad468fe3
TUNG <portal25101999@gmail.com>
2020-03-19 15:11:30 +0700

    tim kiem final

--- TTNhom/TimKiemForm.Designer.cs
+++ TTNhom/TimKiemForm.Designer.cs
@@ -28,9 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TimKiemForm));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btnTimKiem = new System.Windows.Forms.Button();
             this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.LoaiHinhcomboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,27 +52,62 @@
             // btnTimKiem
             // 
             this.btnTimKiem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.btnTimKiem.Location = new System.Drawing.Point(244, 9);
+            this.btnTimKiem.Image = ((System.Drawing.Image)(resources.GetObject("btnTimKiem.Image")));
+            this.btnTimKiem.Location = new System.Drawing.Point(669, 9);
             this.btnTimKiem.Name = "btnTimKiem";
-            this.btnTimKiem.Size = new System.Drawing.Size(109, 29);
+            this.btnTimKiem.Size = new System.Drawing.Size(34, 29);
             this.btnTimKiem.TabIndex = 7;
-            this.btnTimKiem.Text = "Tìm Kiếm";
             this.btnTimKiem.UseVisualStyleBackColor = true;
             this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
             // 
             // txtTimKiem
             // 
             this.txtTimKiem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.txtTimKiem.Location = new System.Drawing.Point(12, 12);
+            this.txtTimKiem.Location = new System.Drawing.Point(385, 12);
             this.txtTimKiem.Name = "txtTimKiem";
-            this.txtTimKiem.Size = new System.Drawing.Size(226, 23);
+            this.txtTimKiem.Size = new System.Drawing.Size(255, 23);
             this.txtTimKiem.TabIndex = 6;
+            // 
+            // LoaiHinhcomboBox
+            // 
+            this.LoaiHinhcomboBox.FormattingEnabled = true;
+            this.LoaiHinhcomboBox.Items.AddRange(new object[] {
+            "Mặt Hàng",
+            "Loại Hàng",
+            "Quầy",
+            "Nhà Cung Cấp"});
+            this.LoaiHinhcomboBox.Location = new System.Drawing.Point(118, 14);
+            this.LoaiHinhcomboBox.Name = "LoaiHinhcomboBox";
+            this.LoaiHinhcomboBox.Size = new System.Drawing.Size(162, 21);
+            this.LoaiHinhcomboBox.TabIndex = 9;
+            this.LoaiHinhcomboBox.SelectedIndexChanged += new System.EventHandler(this.LoaiHinhcomboBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(316, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Từ khóa:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Loại hình:";
             // 
             // TimKiemForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(735, 454);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LoaiHinhcomboBox);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnTimKiem);
             this.Controls.Add(this.txtTimKiem);
@@ -87,5 +126,8 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button btnTimKiem;
         private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.ComboBox LoaiHinhcomboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- TTNhom/TimKiemForm.cs
+++ TTNhom/TimKiemForm.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace TTNhom
 {
@@ -16,14 +17,25 @@ namespace TTNhom
         DBAccess access = new DBAccess();
         DataTable table;
 
-        //private static string strConn = "Data Source=DESKTOP-1NPLUNJ;Initial Catalog=TTCSDL;Integrated Security=True";
+        private static string strConn = "Data Source=DESKTOP-1NPLUNJ;Initial Catalog=TTCSDL;Integrated Security=True";
         private static SqlConnection conn = new SqlConnection(DBAccess.strConn);
         private static SqlDataAdapter adt = new SqlDataAdapter();
         private static SqlCommand cmd = new SqlCommand();
 
+        public bool IsNumber(string pText)
+        {
+            Regex regex = new Regex(@"^[-+]?[0-9]*\.?[0-9]+$");
+            return regex.IsMatch(pText);
+        }
+
         public TimKiemForm()
         {
             InitializeComponent();
+        }
+
+        private void TimKiemForm_Load(object sender, EventArgs e)
+        {
+
         }
 
         public void createConn()
@@ -51,24 +63,109 @@ namespace TTNhom
             conn.Close();
         }
 
+        private void LoaiHinhcomboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            string category_key = LoaiHinhcomboBox.Text.Trim();
+            switch (category_key)
+            {
+                case ("Mặt Hàng"):
+                        GetData("SELECT * FROM MatHang_View", dataGridView1, table);
+                    break;
+                case ("Loại Hàng"):
+                        GetData("SELECT * FROM LoaiHang", dataGridView1, table);
+                    break;
+                case ("Quầy"):
+                        GetData("SELECT * FROM Quay", dataGridView1, table);
+                    break;
+                case ("Nhà Cung Cấp"):
+                        GetData("SELECT * FROM NhaCungCap", dataGridView1, table);
+                    break;
+            }
+        }
+
         private void btnTimKiem_Click(object sender, EventArgs e)
         {
             table = new DataTable();
             string key = txtTimKiem.Text.Trim();
-            if (key.Equals(""))
-            {
-                GetData("SELECT * FROM MatHang_View", dataGridView1, table);
-            }
-            else
-            {
-                string query = "SELECT * FROM MatHang_View WHERE TenMatHang LIKE N'%" + key + "%'";
-                GetData(query, dataGridView1, table);
-            }
-        }
+            string category_key = LoaiHinhcomboBox.Text.Trim();
 
-        private void TimKiemForm_Load(object sender, EventArgs e)
-        {
-
+            switch (category_key)
+            {
+                case ("Mặt Hàng"):
+                    if (key.Equals(""))
+                    {
+                        GetData("SELECT * FROM MatHang_View", dataGridView1, table);
+                    }
+                    else
+                    {
+                        if (IsNumber(key))
+                        {
+                            string queryID = "SELECT * FROM MatHang_View WHERE MaMatHang = '" + key + "' ";
+                            GetData(queryID, dataGridView1, table);
+                        }
+                        string query = 
+                            "SELECT * FROM MatHang_View WHERE TenMatHang LIKE N'%" + key + "%' " +
+                            " OR TenLoaiHang LIKE N'%" + key + "%'" +
+                            " OR TenQuay LIKE N'%" + key + "%'";
+                        GetData(query, dataGridView1, table);
+                    }
+                    break;
+                case ("Loại Hàng"):
+                    if (key.Equals(""))
+                    {
+                        GetData("SELECT * FROM LoaiHang", dataGridView1, table);
+                    }
+                    else
+                    {
+                        if (IsNumber(key))
+                        {
+                            string queryID = "SELECT * FROM LoaiHang WHERE MaLoaiHang = '" + key + "' ";
+                            GetData(queryID, dataGridView1, table);
+                        }
+                        string query = 
+                            "SELECT * FROM LoaiHang WHERE TenLoaiHang LIKE N'%" + key + "%'";
+                        GetData(query, dataGridView1, table);
+                    }
+                    break;
+                case ("Quầy"):
+                    if (key.Equals(""))
+                    {
+                        GetData("SELECT * FROM Quay", dataGridView1, table);
+                    }
+                    else
+                    {
+                        if (IsNumber(key))
+                        {
+                            string queryID = "SELECT * FROM Quay WHERE MaQuay = '" + key + "' ";
+                            GetData(queryID, dataGridView1, table);
+                        }
+                        string query = 
+                            "SELECT * FROM Quay WHERE TenQuay LIKE N'%" + key + "%'" +
+                            " OR MaNhanVien LIKE = '" + key + "'";
+                        GetData(query, dataGridView1, table);
+                    }
+                    break;
+                case ("Nhà Cung Cấp"):
+                    if (key.Equals(""))
+                    {
+                        GetData("SELECT * FROM NhaCungCap", dataGridView1, table);
+                    }
+                    else
+                    {
+                        if (IsNumber(key))
+                        {
+                            string queryID = "SELECT * FROM NhaCungCap WHERE MaNCC = '" + key + "' ";
+                            GetData(queryID, dataGridView1, table);
+                        }
+                        string query = "SELECT * FROM NhaCungCap WHERE TenNCC LIKE N'%" + key + "%'" +
+                            " OR Phuong LIKE N'%" + key + "%'" +
+                            " OR Quan LIKE N'%" + key + "%'" +
+                            " OR ThanhPho LIKE N'%" + key + "%'";
+                        GetData(query, dataGridView1, table);
+                    }
+                    break;
+            }
         }
     }
 }
